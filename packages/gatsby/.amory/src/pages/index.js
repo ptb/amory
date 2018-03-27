ebb609eebdd0f74f0576f57b789ee5eafbbab267
Peter T Bosse II <ptb@ioutime.com>
2018-03-27 07:47:36 -0400

    v2018.3.27-2

--- packages/gatsby/.amory/src/pages/index.js
+++ packages/gatsby/.amory/src/pages/index.js
@@ -19,10 +19,6 @@ export default () =>
 
 // import { css } from "../parts/style.js"
 // import { createElement as h } from "react"
-// import { styled } from "styletron-react"
-
-// const style = (a) => (b) => h (styled ((c) => h (b, c), a))
 
 // export default () =>
-//   style ({ "background-color": "#eee" }) ((a) =>
-//     h ("div", a, h ("p", { "className": css ({ "color": "#f00" }) }, "Hi")))
+//   h ("p", { "className": css ({ "color": "#ff0" }) }, "He")
