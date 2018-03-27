5aa46f8c22e553c169593a899d43c188f9dd1b76
Peter T Bosse II <ptb@ioutime.com>
2018-03-27 11:04:39 -0400

    v2018.3.27-13

--- packages/gatsby/.amory/src/layouts/index.js
+++ packages/gatsby/.amory/src/layouts/index.js
@@ -1,11 +1,12 @@
 import { createElement as h } from "react"
 
-// import transition from "@ptb/animated-transition"
-// import { css } from "@ptb/gatsby-plugin-styletron/style"
-
 export default (a) =>
   h ("div", {}, a.children ())
 
+// import { css } from "@ptb/gatsby-plugin-styletron/style"
+// import { createElement as h } from "react"
+// import transition from "@ptb/animated-transition"
+
+// export default (a) =>
 //   h ("div", { "className": css ({ "overflow": "hidden" }) },
-//     h (transition, a, a.children ())
-//   )
+//     h (transition, a, a.children ()))
