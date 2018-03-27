3c49aa9bfb64f8c0bbc2775a57edf3064111b327
Peter T Bosse II <ptb@ioutime.com>
2018-03-27 13:16:58 -0400

    v2018.3.27-15

--- packages/gatsby/.amory/src/layouts/index.js
+++ packages/gatsby/.amory/src/layouts/index.js
@@ -4,9 +4,11 @@ export default (a) =>
   h ("div", {}, a.children ())
 
 // import { css } from "@ptb/gatsby-plugin-styletron/style"
-// import { createElement as h } from "react"
-// import transition from "@ptb/animated-transition"
+// import React from "react"
+// import Transition from "@ptb/animated-transition"
 
-// export default (a) =>
-//   h ("div", { "className": css ({ "overflow": "hidden" }) },
-//     h (transition, a, a.children ()))
+// export default (a) => pug `
+//   div(class=css({ "overflow": "hidden" }))
+//     Transition(...a)
+//       = a.children ()
+// `
--- packages/gatsby/.amory/src/pages/index.js
+++ packages/gatsby/.amory/src/pages/index.js
@@ -10,9 +10,15 @@ export default () =>
 //   h ("div", { "className": css ({ "color": "#000" }) }, "Hi")
 
 // import { css } from "@ptb/gatsby-plugin-styletron/style"
+// import Link from "gatsby-link"
 // import React from "react"
 
-// export default () =>
-//   pug `
-//     div(className=${css ({ "color": "#000" })}) Hi
-//   `
+// export default () => pug `
+//   Link(to="/page-2/") To Page 2
+//   div(class=css ({ "color": "#000" })) Hi
+// `
+
+// export default () => pug `
+//   Link(to={ "pathname": "/", "state": { "dir": "back" } }) To Index
+//   div(class=css ({ "color": "#000" })) Hi
+// `
