bb0c79d32ecf73cb6cd75484dec3b62ec88ba694
Peter T Bosse II <ptb@ioutime.com>
2018-03-27 15:13:48 -0400

    v2018.3.27-19

--- packages/gatsby/.amory/src/layouts/index.js
+++ packages/gatsby/.amory/src/layouts/index.js
@@ -3,12 +3,15 @@ import { createElement as h } from "react"
 export default (a) =>
   h ("div", {}, a.children ())
 
+// import React, { Fragment } from "react"
 // import { css } from "@ptb/gatsby-plugin-styletron/style"
-// import React from "react"
+// import Helmet from "react-helmet"
 // import Transition from "@ptb/animated-transition"
 
 // export default (a) => pug `
-//   div(class=css({ "overflow": "hidden" }))
-//     Transition(...a)
-//       = a.children ()
+//   Fragment
+//     Helmet(bodyAttributes=({ "class": css ({ "margin": "0" })}))
+//     div(class=css({ "overflow": "hidden" }))
+//       Transition(...a)
+//         = a.children ()
 // `
