d24e4fbaf3abc151078373df3b20daeda49c2fee
Peter T Bosse II <ptb@ioutime.com>
2018-04-06 14:49:08 -0400

    v2018.4.6-8

--- packages/gatsby/.amory/src/layouts/index.js
+++ packages/gatsby/.amory/src/layouts/index.js
@@ -4,14 +4,17 @@ export default (a) =>
   h ("div", {}, a.children ())
 
 // import React, { Fragment } from "react"
-// import { body } from "@ptb/animated-transition/styles.json"
-// import { css } from "@ptb/gatsby-plugin-styletron/style"
 // import Helmet from "react-helmet"
+// import styletron from "@ptb/gatsby-plugin-styletron"
 // import Transition from "@ptb/animated-transition"
 
-// export default (a) => pug `
-//   Fragment
-//     Helmet(bodyAttributes=({ "class": css (body) }))
-//     Transition(...a)
-//       = a.children ()
-// `
+// export default (a) => {
+//   const css = styletron ().css
+
+//   return pug `
+//     Fragment
+//       Helmet(bodyAttributes=({ "class": css ({ "borderColor": "#eee" }) }))
+//       Transition(...a)
+//         = a.children ()
+//   `
+// }
