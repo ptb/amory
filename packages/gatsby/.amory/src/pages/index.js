fcfd4956415b5a571016c5dc5bb3a16cb6b7521b
Peter T Bosse II <ptb@ioutime.com>
2018-04-07 07:52:57 -0400

    v2018.4.7-0

--- packages/gatsby/.amory/src/pages/index.js
+++ packages/gatsby/.amory/src/pages/index.js
@@ -3,32 +3,43 @@ import { createElement as h } from "react"
 export default () =>
   h ("div", {}, "Hi")
 
-// import { css } from "@ptb/gatsby-plugin-styletron/style"
+// import styletron from "@ptb/gatsby-plugin-styletron"
 // import { createElement as h } from "react"
 
-// export default () =>
-//   h ("div", { "className": css ({ "color": "#000" }) }, "Hi")
+// export default () => {
+//   const css = styletron ().css
 
-// import { css } from "@ptb/gatsby-plugin-styletron/style"
+//   return h ("div", { "className": css ({ "color": "#000" }) }, "Hi")
+// }
+
+// import styletron from "@ptb/gatsby-plugin-styletron"
 // import Link from "gatsby-link"
 // import React from "react"
-// import { View } from "@ptb/animated-transition"
+// import { view } from "@ptb/animated-transition/styles.json"
 
-// export default () => pug `
-//   View(style=({ "background-color": "rgba(255,204,204,0.8)" }))
-//     Link(to=({ "pathname": "/page-2/", "state": { "anim": "slide.fore" } })) To Page 2
-//     Link(to=({ "pathname": "/page-2/" })) To Page 2 (without transition)
-//     Link(to="/page-2/") To Page 2 (without transition 2)
-//     div(class=css ({ "color": "#000" })) Hi
-// `
+// export default () => {
+//   const css = styletron ().css
 
-// export default () => pug `
-//   View(style=({ "background-color": "rgba(204,255,204,0.8)" }))
-//     Link(to=({ "pathname": "/", "state": { "anim": "slide.back" } })) To Index
-//     Link(to=({ "pathname": "/" })) To Index (without transition)
-//     Link(to="/") To Index (without transition 2)
-//     div(class=css ({ "color": "#000" })) Hi
-// `
+//   return pug `
+//     div(class=css (view) style=({ "background-color": "rgba(255,204,204,0.8)" }))
+//       Link(to=({ "pathname": "/page-2/", "state": { "anim": "slide.fore" } })) To Page 2
+//       Link(to=({ "pathname": "/page-2/" })) To Page 2 (without transition)
+//       Link(to="/page-2/") To Page 2 (without transition 2)
+//       div(class=css ({ "color": "#000" })) Hi
+//   `
+// }
+
+// export default () => {
+//   const css = styletron ().css
+
+//   return pug `
+//     div(class=css (view) style=({ "background-color": "rgba(204,255,204,0.8)" }))
+//       Link(to=({ "pathname": "/", "state": { "anim": "slide.back" } })) To Index
+//       Link(to=({ "pathname": "/" })) To Index (without transition)
+//       Link(to="/") To Index (without transition 2)
+//       div(class=css ({ "color": "#000" })) Hi
+//   `
+// }
 
 // import Img from "@ptb/gatsby-image"
 
