8191cfee160c03664720a5268375c18fa5ac7e79
Peter T Bosse II <ptb@ioutime.com>
2018-04-26 09:15:23 -0400

    v2018.4.26-4

--- packages/gatsby/.amory/src/pages/index.js
+++ packages/gatsby/.amory/src/pages/index.js
@@ -1,35 +1,23 @@
-import { createElement as h } from "react"
+import Img from "@ptb/gatsby-image"
+import Link from "gatsby-link"
+import React from "react"
+import styletron from "@ptb/gatsby-plugin-styletron"
+import { view } from "@ptb/animated-transition/styles.json"
 
-export default () =>
-  h ("div", {}, "Hi")
+export default (a) => {
+  const css = styletron ().css
 
-// import styletron from "@ptb/gatsby-plugin-styletron"
-// import { createElement as h } from "react"
+  return pug `
+    div(class=css (view) style=({ "backgroundColor": "rgba(255,204,204,0.8)" }))
+      Link(to=({ "pathname": "/page-2/", "state": { "anim": "slide.fore" } })) To Page 2
+      Link(to=({ "pathname": "/page-2/" })) To Page 2 (without transition)
+      Link(to="/page-2/") To Page 2 (without transition 2)
+      div(class=css ({ "color": "#000" }))
+        Img(resolutions=a.data.hero.childImageSharp.desktop)
+  `
+}
 
-// export default () => {
-//   const css = styletron ().css
-
-//   return h ("div", { "className": css ({ "color": "#000" }) }, "Hi")
-// }
-
-// import Link from "gatsby-link"
-// import React from "react"
-// import styletron from "@ptb/gatsby-plugin-styletron"
-// import { view } from "@ptb/animated-transition/styles.json"
-
-// export default () => {
-//   const css = styletron ().css
-
-//   return pug `
-//     div(class=css (view) style=({ "backgroundColor": "rgba(255,204,204,0.8)" }))
-//       Link(to=({ "pathname": "/page-2/", "state": { "anim": "slide.fore" } })) To Page 2
-//       Link(to=({ "pathname": "/page-2/" })) To Page 2 (without transition)
-//       Link(to="/page-2/") To Page 2 (without transition 2)
-//       div(class=css ({ "color": "#000" })) Hi
-//   `
-// }
-
-// export default () => {
+// export default (a) => {
 //   const css = styletron ().css
 
 //   return pug `
@@ -37,24 +25,19 @@ export default () =>
 //       Link(to=({ "pathname": "/", "state": { "anim": "slide.back" } })) To Index
 //       Link(to=({ "pathname": "/" })) To Index (without transition)
 //       Link(to="/") To Index (without transition 2)
-//       div(class=css ({ "color": "#000" })) Hi
+//       div(class=css ({ "color": "#333" }))
+//         Img(resolutions=a.data.hero.childImageSharp.desktop)
 //   `
 // }
 
-// import Img from "@ptb/gatsby-image"
-
-// export default (a) =>
-//   h ("div", {},
-//     h (Img, { "resolutions": a.data.hero.childImageSharp.desktop }))
-
-// export const query = graphql `
-//   query GatsbyImageSunQuery {
-//     hero: file(relativePath: {eq: "sun.png"}) {
-//       childImageSharp {
-//         desktop: resolutions(width: 125, height: 125, quality: 80) {
-//           ...GatsbyImageSharpResolutions_withWebp_tracedSVG
-//         }
-//       }
-//     }
-//   }
-// `
+export const query = graphql `
+  query GatsbyImageSunQuery {
+    hero: file(relativePath: {eq: "sun.png"}) {
+      childImageSharp {
+        desktop: resolutions(width: 125, height: 125, quality: 80) {
+          ...GatsbyImageSharpResolutions_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`
