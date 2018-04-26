6338127d2599dea8c1f90d1c8981ac2818faa6a4
Peter T Bosse II <ptb@ioutime.com>
2018-04-26 07:22:15 -0400

    v2018.4.26-0

--- packages/gatsby-plugin-pug/.amory/html.js
+++ packages/gatsby-plugin-pug/.amory/html.js
@@ -17,32 +17,32 @@ const styles = pug `
       box-sizing: inherit
 `.trim ()
 
-// export default (props) => pug `
-//   html(...props.htmlAttributes)
-//     head
-//       meta(charset="utf-8")/
-//       meta(content="ie=edge" httpEquiv="x-ua-compatible")/
-//       meta(content="initial-scale=1,width=device-width" name="viewport")/
-//       style= styles
-//       = props.headComponents
-//     body(...props.bodyAttributes)
-//       = props.preBodyComponents
-//       #root(dangerouslySetInnerHTML=({ "__html": props.body }) key="body")
-//       = props.postBodyComponents
-// `
+export default (props) => pug `
+  html(...props.htmlAttributes)
+    head
+      meta(charset="utf-8")/
+      meta(content="ie=edge" httpEquiv="x-ua-compatible")/
+      meta(content="initial-scale=1,width=device-width" name="viewport")/
+      style= styles
+      = props.headComponents
+    body(...props.bodyAttributes)
+      = props.preBodyComponents
+      #root(dangerouslySetInnerHTML=({ "__html": props.body }) key="body")
+      = props.postBodyComponents
+`
 
-export default (props) =>
-  h ("html", props.htmlAttributes,
-    h ("head", {},
-      h ("meta", { "charset": "utf-8" }),
-      h ("meta", { "content": "ie=edge", "httpEquiv": "x-ua-compatible" }),
-      h ("meta", { "content": "initial-scale=1,width=device-width", "name": "viewport" }),
-      h ("style", {}, styles),
-      props.headComponents
-    ),
-    h ("body", props.bodyAttributes,
-      props.preBodyComponents,
-      h ("div", { "dangerouslySetInnerHTML": { "__html": props.body }, "id": "root", "key": "body" }),
-      props.postBodyComponents
-    )
-  )
+// export default (props) =>
+//   h ("html", props.htmlAttributes,
+//     h ("head", {},
+//       h ("meta", { "charset": "utf-8" }),
+//       h ("meta", { "content": "ie=edge", "httpEquiv": "x-ua-compatible" }),
+//       h ("meta", { "content": "initial-scale=1,width=device-width", "name": "viewport" }),
+//       h ("style", {}, styles),
+//       props.headComponents
+//     ),
+//     h ("body", props.bodyAttributes,
+//       props.preBodyComponents,
+//       h ("div", { "dangerouslySetInnerHTML": { "__html": props.body }, "id": "root", "key": "body" }),
+//       props.postBodyComponents
+//     )
+//   )
