16527c051e422cd2e513bea34e11dd6fc205fa37
Peter T Bosse II <ptb@ioutime.com>
2018-04-29 13:35:11 -0400

    v2018.4.29-12

---
+++ packages/graphql-filesystem/extend-file-node.js
@@ -0,0 +1,41 @@
+const { GraphQLString } = require ("graphql")
+const fs = require ("fs-extra")
+const path = require ("path")
+
+const staticPath = path.join (process.cwd (), "public", "static")
+
+module.exports = ({ getNodeAndSavePathDependency, pathPrefix = "", type }) =>
+  (type.name === "File"
+    ? {
+      "publicURL": {
+        "args": {},
+        "description":
+            "Copy file to static directory and return its public URL",
+        "resolve": (file, _fieldArgs, context) => {
+          const details = getNodeAndSavePathDependency (
+            file.id,
+            context.path
+          )
+          const digest = file.internal.contentDigest.slice (0, 6)
+          const fileName = `${file.name}-${digest}${details.ext}`
+          const publicPath = path.join (staticPath, fileName)
+
+          if (!fs.existsSync (publicPath)) {
+            fs.copy (details.absolutePath, publicPath, (err) => {
+              if (err) {
+                console.error (
+                  `error copying "${
+                    details.absolutePath
+                  }" to "${publicPath}"`,
+                  err
+                )
+              }
+            })
+          }
+
+          return `${pathPrefix}/static/${fileName}`
+        },
+        "type": GraphQLString
+      }
+    }
+    : {})
---
+++ packages/graphql-filesystem/index.js
@@ -0,0 +1,5 @@
+const fs = require ("fs-extra")
+
+exports.createFilePath = require ("./create-file-path")
+exports.createRemoteFileNode = require ("./create-remote-file-node")
+exports.loadNodeContent = (file) => fs.readFile (file.absolutePath, "utf-8")
