143c7a736312f36cac19628709ebe7e172fa4d52
Peter T Bosse II <ptb@ioutime.com>
2018-04-29 07:15:35 -0400

    v2018.4.29-1

--- packages/create-pages/index.js
+++ packages/create-pages/index.js
@@ -2,11 +2,16 @@ const { extname, join, parse, relative } = require ("path").posix
 const includes = require ("lodash.includes")
 const slash = require ("slash")
 
-const canProcess = (exts, src, pages) => [
-  includes (exts, extname (src)) &&
-    !(/^(_|template-)|__tests__|(\.d\.t|(spec|test)\.j)sx?$/).test (src),
-  !includes (pages.reduce ((a, b) => a.concat (b.component), []), src)
-]
+const canProcess = (exts, src, pages) => {
+  const { dir, name } = parse (slash (src))
+
+  return [
+    includes (exts, extname (src)) &&
+      !(/__tests__/).test (dir) &&
+      !(/^(_|template-)|(\.d\.t|(spec|test)\.j)sx?$/).test (name),
+    !includes (pages.reduce ((a, b) => a.concat (b.component), []), src)
+  ]
+}
 
 const getPage = (base, src, component) => {
   const { dir, name } = parse (relative (slash (base), slash (src)))
