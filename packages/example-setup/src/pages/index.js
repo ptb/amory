2b68f1f0ccbe19142ca79669162afd537aea0d4a
Peter T Bosse II <ptb@ioutime.com>
2018-04-29 17:33:17 -0400

    v2018.4.29-19

--- packages/example-setup/src/pages/index.js
+++ packages/example-setup/src/pages/index.js
@@ -1,5 +1,24 @@
+import Img from "@amory/image-lazyload"
 import React from "react"
+import styletron from "@amory/style-styletron"
 
-export default () => pug `
-  div Hi
+export default (a) => {
+  const css = styletron ().css
+
+  return pug `
+    div(class=css ({ "color": "#333" }))
+      Img(resolutions=a.data.hero.childImageSharp.desktop)
+  `
+}
+
+export const query = graphql `
+  query GatsbyImageSunQuery {
+    hero: file(relativePath: {eq: "sun.png"}) {
+      childImageSharp {
+        desktop: resolutions(width: 125, height: 125, quality: 80) {
+          ...GatsbyImageSharpResolutions_withWebp_tracedSVG
+        }
+      }
+    }
+  }
 `
