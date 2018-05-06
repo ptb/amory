beea80d01248641f051b1f195779c23b73531b53
Peter T Bosse II <ptb@ioutime.com>
2018-05-06 07:28:02 -0400

    v2018.5.6-4

---
+++ packages/schema-filesystem/image.js
@@ -0,0 +1,44 @@
+const getFileNode = require ("./node")
+const sharp = require ("sharp")
+const watcher = require ("./watch")
+
+module.exports = async (
+  { "actions": { createNode, deleteNode }, createNodeId, emitter, getNode },
+  opts = {}
+) => {
+  const addFile = async ({ src }) => {
+    const node = Object.assign ({},
+      await getFileNode ({
+        createNodeId,
+        opts,
+        src,
+        "type": "Image"
+      }),
+      await sharp (src).metadata ())
+
+    createNode (node)
+  }
+
+  const delFile = ({ src }) => {
+    const node = getNode (createNodeId (src))
+
+    if (node) {
+      deleteNode (node.id, node)
+    }
+  }
+
+  const modFile = async ({ src }) => {
+    delFile (src)
+    addFile (src)
+  }
+
+  const fn = {
+    "addFile": [addFile, "GQL_FS_IMG_ADD"],
+    "delFile": [delFile, "GQL_FS_IMG_DEL"],
+    "modFile": [modFile, "GQL_FS_IMG_MOD"],
+    "regex": /\.(gif|jpe?g|png|svg|tiff?|webp)$/i,
+    "src": opts.src || opts.path || "src/images"
+  }
+
+  await watcher (fn, emitter)
+}
