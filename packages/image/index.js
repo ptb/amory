0a5905591880181d059caf871560be274cf0a7e3
Peter T Bosse II <ptb@ioutime.com>
2018-05-21 10:47:06 -0400

    v2018.5.21-1

--- packages/image/index.js
+++ packages/image/index.js
@@ -2,56 +2,16 @@ import styletron from "@amory/styletron"
 import { Component, createElement as h } from "react"
 
 if (typeof window !== "undefined") {
-  require ("intersection-observer")
   require ("picturefill")
-  require ("weakmap-polyfill")
 }
-
-const ioObservers = new WeakMap ()
 
 class Img extends Component {
   constructor (props) {
     super (props)
 
-    this.hasIOApi = Boolean (
-      typeof window !== "undefined" && window.IntersectionObserver
-    )
-
-    if (this.hasIOApi) {
-      this.ioInstance = new IntersectionObserver (Img.ioEvents, {
-        "rootMargin": "200px"
-      })
-    }
-
-    let isLoaded = true
-    let isVisible = true
-
-    if (this.hasIOApi || typeof window === "undefined") {
-      isLoaded = false
-      isVisible = false
-    }
-
     this.state = {
-      "isLoaded": isLoaded,
-      "isVisible": isVisible
+      "isLoaded": typeof window === "undefined"
     }
-
-    this.ioObserve = this.ioObserve.bind (this)
-  }
-
-  ioObserve (ref) {
-    if (this.hasIOApi && ref) {
-      ioObservers.set (ref, () => this.setState ({ "isVisible": true }))
-      this.ioInstance.observe (ref)
-    }
-  }
-
-  static ioEvents (entries, ioInstance) {
-    entries.filter ((entry) => entry.isIntersecting).forEach ((entry) => {
-      ioObservers.get (entry.target) ()
-      ioObservers.delete (entry.target)
-      ioInstance.unobserve (entry.target)
-    })
   }
 
   image (isLoaded, primary) {
@@ -60,13 +20,11 @@ class Img extends Component {
     return h ("img", {
       "alt": this.props.title,
       "className": css ({
-        "height": "100%",
         "objectFit": "cover",
         "opacity": isLoaded ? 1 : 0,
         "position": "absolute",
         "transitionDuration": ".35s",
         "transitionProperty": "opacity",
-        "width": "100%"
       }),
       "onload": primary
         ? (() => !this.state.isLoaded && this.setState ({ "isLoaded": true })) ()
@@ -82,13 +40,13 @@ class Img extends Component {
         (image.media
           ? {
             [`@media ${image.media}`]: {
-              "maxHeight": `${image.height}px`,
-              "maxWidth": `${image.width}px`
+              "height": `${image.height}px`,
+              "width": `${image.width}px`
             }
           }
           : {
-            "maxHeight": `${image.height}px`,
-            "maxWidth": `${image.width}px`
+            "height": `${image.height}px`,
+            "width": `${image.width}px`
           })
     )
 
@@ -96,15 +54,12 @@ class Img extends Component {
       "className": css (
         Object.assign (
           {
-            "flexGrow": 1,
             "overflowX": "hidden",
-            "position": "relative",
-            "width": "100%"
+            "position": "relative"
           },
           ... cssmq
         )
       ),
-      "ref": this.ioObserve,
       "title": this.props.title
     }
   }
@@ -135,12 +90,11 @@ class Img extends Component {
         this.image (!this.state.isLoaded, false)
       ),
 
-      this.state.isVisible &&
-        h ("picture", {},
-          Object.values (this.props.images).map ((i = {}) =>
-            this.sources ([i.webp, i.jpg, i.png], i.media)),
-          this.image (this.state.isLoaded, true)
-        ),
+      h ("picture", {},
+        Object.values (this.props.images).map ((i = {}) =>
+          this.sources ([i.webp, i.jpg, i.png], i.media)),
+        this.image (this.state.isLoaded, true)
+      ),
 
       ... children
     )
