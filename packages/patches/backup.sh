fdbba28eb68233da3b91565db320357bc2114ece
Peter T Bosse II <ptb@ioutime.com>
2018-06-12 08:12:47 -0400

    v2018.6.12-1

---
+++ packages/patches/backup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+cp node_modules/babel-plugin-remove-graphql-queries/index.js \
+node_modules/babel-plugin-remove-graphql-queries/index.js.orig
+cp node_modules/gatsby/cache-dir/app.js \
+node_modules/gatsby/cache-dir/app.js.orig
+cp node_modules/gatsby/cache-dir/default-html.js \
+node_modules/gatsby/cache-dir/default-html.js.orig
+cp node_modules/gatsby/cache-dir/develop-static-entry.js \
+node_modules/gatsby/cache-dir/develop-static-entry.js.orig
+cp node_modules/gatsby/cache-dir/loader.js \
+node_modules/gatsby/cache-dir/loader.js.orig
+cp node_modules/gatsby/cache-dir/production-app.js \
+node_modules/gatsby/cache-dir/production-app.js.orig
+cp node_modules/gatsby/cache-dir/root.js \
+node_modules/gatsby/cache-dir/root.js.orig
+cp node_modules/gatsby/cache-dir/static-entry.js \
+node_modules/gatsby/cache-dir/static-entry.js.orig
+cp node_modules/gatsby/dist/bootstrap/index.js \
+node_modules/gatsby/dist/bootstrap/index.js.orig
+cp node_modules/gatsby/dist/bootstrap/load-plugins/load.js \
+node_modules/gatsby/dist/bootstrap/load-plugins/load.js.orig
+cp node_modules/gatsby/dist/internal-plugins/query-runner/query-runner.js \
+node_modules/gatsby/dist/internal-plugins/query-runner/query-runner.js.orig
+cp node_modules/gatsby/dist/redux/reducers/webpack.js \
+node_modules/gatsby/dist/redux/reducers/webpack.js.orig
+cp node_modules/gatsby/dist/utils/copy-static-directory.js \
+node_modules/gatsby/dist/utils/copy-static-directory.js.orig
+cp node_modules/gatsby/dist/utils/js-chunk-names.js \
+node_modules/gatsby/dist/utils/js-chunk-names.js.orig
+cp node_modules/gatsby/dist/utils/webpack-utils.js \
+node_modules/gatsby/dist/utils/webpack-utils.js.orig
+cp node_modules/gatsby/dist/utils/webpack.config.js \
+node_modules/gatsby/dist/utils/webpack.config.js.orig
+cp node_modules/gatsby/dist/utils/websocket-manager.js \
+node_modules/gatsby/dist/utils/websocket-manager.js.orig
+cp node_modules/react-dom/cjs/react-dom-server.node.development.js \
+node_modules/react-dom/cjs/react-dom-server.node.development.js.orig
