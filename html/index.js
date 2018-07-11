3c14f29396a330d5a1a3a28d7c4898314bcb0043
Peter T Bosse II <ptb@ioutime.com>
2018-07-11 14:56:40 -0400

    v2018.7.11-1

--- html/index.js
+++ html/index.js
@@ -1,36 +1,31 @@
 const merge = require ("deepmerge")
 const HtmlWebpackPlugin = require ("html-webpack-plugin")
-const HtmlWebpackTemplate = require ("html-webpack-template")
+const nodeDir = require ("node-dir")
+const { relative } = require ("path").posix
 
-module.exports = (config) =>
-  config.module
-    .rule ("html")
-    .test (/\.html$/)
-    .exclude.add (/node_modules/)
-    .end ()
-    .use ("html")
-    .loader (require.resolve ("html-loader"))
-    .tap ((options = {}) =>
-      merge (options, {
-        "minimize": true
-      }))
-    .end ()
-    .end ()
-    .end ()
-    .plugin ("html")
-    .use (HtmlWebpackPlugin)
-    .tap ((options = {}) => [
-      merge (options, {
-        "appMountId": "root",
-        "inject": false,
-        "minify": {
-          "collapseWhitespace": true,
-          "keepClosingSlash": true,
-          "preserveLineBreaks": true,
-          "useShortDoctype": true
-        },
-        "mobile": true,
-        "template": HtmlWebpackTemplate,
-        "xhtml": true
-      })
-    ])
+module.exports = (config) => {
+  const dir = config.output.get ("path")
+
+  nodeDir
+    .files (dir, { "sync": true })
+    .filter ((asset) => (/\.(html?)$/).test (asset))
+    .forEach ((asset) =>
+      config
+        .plugin (asset)
+        .use (HtmlWebpackPlugin)
+        .tap ((options = {}) => [
+          merge (options, {
+            "filename": relative (dir, asset),
+            "minify": {
+              "collapseWhitespace": true,
+              "keepClosingSlash": true,
+              "preserveLineBreaks": true,
+              "useShortDoctype": true
+            },
+            "template": asset,
+            "xhtml": true
+          })
+        ]))
+
+  return config
+}
