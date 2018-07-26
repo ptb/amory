23425e3c99bbe5690ad519d2eaad83a541a23754
Peter T Bosse II <ptb@ioutime.com>
2018-07-26 12:58:56 -0400

    v2018.7.26-4

--- picture/loader.js
+++ picture/loader.js
@@ -97,7 +97,7 @@ module.exports = async function (source, map, meta) {
   const filename = join (
     this._compiler.outputPath,
     filepath,
-    `${opts.input.name}.json`
+    `${opts.input.name}-${opts.input.hash}.json`
   )
 
   let output
--- picture/loader.mjs
+++ picture/loader.mjs
@@ -97,7 +97,7 @@ const loader = async function (source, map, meta) {
   const filename = join (
     this._compiler.outputPath,
     filepath,
-    `${opts.input.name}.json`
+    `${opts.input.name}-${opts.input.hash}.json`
   )
 
   let output
