d4a2471844da7faa16567f9c0c203973a6b07577
Peter T Bosse II <ptb@ioutime.com>
2018-07-27 13:02:49 -0400

    v2018.7.27-2

---
+++ entry/index.js
@@ -0,0 +1,24 @@
+const { ensureDirSync } = require ("fs-extra")
+const { resolve } = require ("path").posix
+const Config = require ("webpack-chain")
+
+const dest = (context, mode) => {
+  const dir = resolve (
+    context,
+    mode === "development" ? "dev" : "web"
+  )
+
+  ensureDirSync (dir)
+
+  return dir
+}
+
+module.exports = ({
+  config = new Config (),
+  context = resolve (process.cwd ()),
+  mode = "production"
+}) =>
+  config
+    .context (context)
+    .mode (mode)
+    .output.path (dest (context, mode))
